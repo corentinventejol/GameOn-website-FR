6421151cf10037a6b8b0bd4c58a2c4cb2c73a67c
corentinventejol <corentinventejol78@gmail.com>
2024-02-13 10:50:18 +0100

    fermé le modal :100:
     je ferme le modal en cliquant sur la croix grâce a un eventListener au click sa utilise la fonction closeModalHandler qui met display none se qui a pour effet de le fermé

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -18,6 +18,17 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+  
+  // Fermer le modal en cliquant sur la croix grace a un eventListener qui attend un clique pour fermer le modal 
+  const closeModal = document.querySelector(".close");
+  if (closeModal) {
+    closeModal.addEventListener("click", closeModalHandler);
+  }
+}
+
+// Fonction pour fermer le modal avec display none
+function closeModalHandler() {
+  modalbg.style.display = "none";
 }
 
 
